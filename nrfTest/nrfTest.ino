f469f4fd2fc4e6ea5033601a62cbfaf7ae5374af
maditnerd <remi@madnerd.org>
2016-01-21 16:17:39 +0100

    First autocommit by pigetBot

---
+++ nrfTest/nrfTest.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include <RF24.h>
+#include "printf.h"
+
+// SETTINGS
+#define CE_PIN 9
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+void setup()
+{
+  Serial.begin(115200);
+  printf_begin();
+  Serial.println("TEST");
+  radio.begin();
+  radio.printDetails();
+}
+
+void loop()
+{
+}
